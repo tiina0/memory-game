5ffca1671473799c8c1ad45360a56ea6f867b469
tiina <tiinamav@gmail.com>
2021-03-31 22:51:46 +0300

    fix play again button

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,4 @@
-// This version works - game starts only after pressing the start button
-// when game finishes, play again? button appears but nothing happens when clickin on it
-// issues: how to "reset game"?
-
+// This version works but after finishing first round, the cards are added twice to the array on the second round.
 
 // select and store cards in an array called cards
 let cards = document.getElementsByClassName("card");
@@ -10,7 +7,7 @@ let startBtn = document.getElementById("start");
 let message = document.querySelector("h2");
 let cardContainer = document.querySelector(".container");
 let playAgainBtn = document.getElementById("playAgain");
-// temporary storage for backgrounds  
+// temporary storage for backgrounds tp be able to compare them 
 let tempStorage = [];
 // temporary storage for elements ( for what ? )
 let tempStorage2 = [];
@@ -61,88 +58,104 @@ function generateArrForRandomColors(num) {
 }
 
 // set of randomColors with 6 unique colors
-let randomColorsArray = generateArrForRandomColors(6);
 
-
+// generate set of cards 
+// function randomizeColorsOnCards(cards, size) {
 function randomizeColorsOnCards() {
+    let randomColorsArray = generateArrForRandomColors(6);
     for (let i = 0; i < cards.length; i++) {
-        for (let i = 0; i < randomColorsArray.length; i++) {
-            cards[i].style.backgroundColor = randomColorsArray[i];
-        }
+        cards[i].style.backgroundColor = randomColorsArray[i];
     }
+    console.log(randomColorsArray);
 }
 
-function reveal(el) {
-    let element = el;
-    tempStorage2.push(element);
-    tempStorage.push(element.parentElement.style.backgroundColor);
-    console.log(tempStorage)
-    element.style.display = "none";
+function reveal(element) {
+    // below code will make it possible to add a card after the array contains one item, making it possible to add only two
+    if (tempStorage.length <= 1 && tempStorage2.length <= 1) {
+        element.style.zIndex = "-10";
+        tempStorage2.push(element);
+        console.log(tempStorage)
+        tempStorage.push(element.parentElement.style.backgroundColor);
+        console.log(tempStorage)
+    }
 }
 
 function hideCards() {
-    tempStorage2[0].style.display = "initial";
-    tempStorage2[1].style.display = "initial";
+    for (let i = 0; i < tempStorage2.length; i++) {
+        tempStorage2[i].style.zIndex = "10";
+    }
     tempStorage = [];
     tempStorage2 = [];
 }
 
-startBtn.addEventListener("click", function() {
+// Try using variables instead of arrays for storing the colors
+function checkIfMatch() {
+    if (tempStorage[0] === tempStorage[1]) {
+        message.textContent = "It's a match";
+        tracker = tracker + 1;
+        console.log(tracker);
+        tempStorage = [];
+        tempStorage2 = [];
+    } else {
+        message.textContent = "Not a match";
+        setTimeout(function () {
+            hideCards();
+        }, 1000);
+    }
+}
 
-    tempStorage = [];
-    tempStorage2 = [];
-    
-    randomizeColorsOnCards();
-    console.log(randomColorsArray);
 
-    this.style.display = "none";
-    message.textContent = "Good Luck!";
-    cardContainer.style.visibility = "visible";
+function resetTracker() {
+    if (tracker === 6) {
+        tracker = 0;
+        message.textContent = "All pairs found, well done!";
+        playAgainBtn.style.visibility = "visible";
+    }
+}
 
+function reset() {
+    playAgainBtn.style.visibility = "hidden";
+    startBtn.style.display = "initial";
+    message.textContent = "";
+    cardContainer.style.visibility = "hidden";
+}
 
+function doStuffWhenCardGetsClicked() {
     for (let i = 0; i < overlay.length; i++) {
         overlay[i].addEventListener("click", function () {
             reveal(this);
-            if (tempStorage.length === 2) {
-                if (tempStorage[0] === tempStorage[1]) {
-                    message.textContent = "It's a match";
-                    tempStorage = [];
-                    tempStorage2 = [];
-                    tracker++;
-                } else {
-                    message.textContent = "Not a match";
-                    setTimeout(function () {
-                        hideCards();
-                    }, 1000);
-                }
+            if (tempStorage.length == 2) {
+                checkIfMatch();
+                resetTracker();
             }
-            if (tracker === 6) {
-                message.textContent = "All pairs found, well done!";
-                playAgainBtn.style.visibility = "visible";
-                tracker = 0;
-            }
-        })
+        });
     }
-})
+    console.log(tempStorage);
+}
 
-// function playAgain() {
-//     playAgainBtn.style.visibility = "hidden";
-//     message.textContent = "";
-//     cardContainer.style.visibility = "hidden";
-//     startBtn.style.display = "initial";
-// }
+startBtn.addEventListener("click", function () {
+    console.log(tracker);
+    randomizeColorsOnCards();
+    cardContainer.style.visibility = "visible";
+    // console.log(randomColorsArray);
+    this.style.display = "none";
+    message.textContent = "Good Luck!";
+    for (let i = 0; i < overlay.length; i++) {
+        overlay[i].style.zIndex = "10";
+    }
+    doStuffWhenCardGetsClicked();
+});
 
-// function restore() {
-//     for(let i = 0; i < overlay.length; i++) {
-//         overlay[i].style.display = "flex";
-//     }
-// }
+playAgainBtn.addEventListener("click", function () {
+    reset();
+    console.log(tempStorage, tempStorage2);
+});
 
-// playAgainBtn.addEventListener("click", function() {
-//     playAgain();
-//     restore();
-// });
 
-// create function to restore cards to initial styles
+
+
+
+
+
 
 
